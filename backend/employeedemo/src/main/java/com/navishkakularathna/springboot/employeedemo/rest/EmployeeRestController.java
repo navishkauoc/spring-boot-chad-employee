0c5f4e4832fff8dcd02bcfcf81d77dd4d7c1b2d6
navishkauoc <navishka.randika@gmail.com>
2020-12-06 14:50:09 +0530

    Get a single employee

--- backend/employeedemo/src/main/java/com/navishkakularathna/springboot/employeedemo/rest/EmployeeRestController.java
+++ backend/employeedemo/src/main/java/com/navishkakularathna/springboot/employeedemo/rest/EmployeeRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +27,18 @@ public class EmployeeRestController {
 	@GetMapping("/employees")
 	public List<Employee> findAll() {
 		return employeeService.findAll();
+	}
+
+	// Get a single employee
+	@GetMapping("/employees/{employeeId}")
+	public Employee getEmployee(@PathVariable int employeeId) {
+		Employee employee = employeeService.findById(employeeId);
+
+		if (employee == null) {
+			throw new RuntimeException("Cannot find an Employee by the given ID: " + employeeId);
+		}
+
+		return employee;
 	}
 
 }
